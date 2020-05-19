917ecf186ab2e672160d653bbe5d948e51fdd4ab
Jaime <jaime_bondgg@hotmail.com>
2020-05-19 12:23:04 +0200

    UPDATE: PlayerAcabado

---
+++ JuegoZombie/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+    public int life;
+    public int stamina;
+    public int left;
+    public int right;
+    public int remain;
+    private int contador;
+    [SerializeField] Text lifeT;
+    [SerializeField] Text staminaT;
+    [SerializeField] Text leftT;
+    [SerializeField] Text rightT;
+    [SerializeField] Text remainT;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       life = 100;
+       stamina = 150;
+       left = 20;
+       right = 20;
+       remain = 10;
+       contador = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (stamina != 150)
+        {
+            contador++;
+            if (contador == 50)
+            {
+                stamina++;
+                contador = 0;
+            }
+        }
+        leftT.GetComponent<Text>().text = left.ToString();
+        rightT.GetComponent<Text>().text = right.ToString();
+        staminaT.GetComponent<Text>().text = stamina.ToString();
+    }
+}
--- JuegoZombie/Assets/Scripts/PlayerManagement.cs
+++ JuegoZombie/Assets/Scripts/PlayerManagement.cs
@@ -8,6 +8,7 @@ public class PlayerManagement : MonoBehaviour
     private Vector2 axis;
     private CharacterController controller;      
     public float speed;
+    private float defaultSpeed;
     public Vector3 moveDirection;
     public float jumpSpeed;
     private bool jump;
@@ -16,6 +17,7 @@ public class PlayerManagement : MonoBehaviour
     private Vector3 transformDirection;
     public GameObject mano;
     public GameObject lanzacohetes;
+    public GameObject manager;
 
 
     [SerializeField] private FireTemplate bullet;
@@ -24,10 +26,21 @@ public class PlayerManagement : MonoBehaviour
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        defaultSpeed = speed;
     }
 
     void Update()
-    {          
+    {    
+        if (Input.GetKey(KeyCode.R) && manager.GetComponent<LevelManager>().stamina >= 1)
+        {
+            speed = defaultSpeed + 10;
+            manager.GetComponent<LevelManager>().stamina = manager.GetComponent<LevelManager>().stamina - 1;
+        }
+        else
+        {
+            speed = defaultSpeed;
+        }
+
         transformDirection = axis.x * transform.right + axis.y * transform.forward;
 
         moveDirection.x = transformDirection.x * speed;
@@ -55,28 +68,36 @@ public class PlayerManagement : MonoBehaviour
 
     public void Fire()
     {
-        Debug.Log("Fire");
+        if (manager.GetComponent<LevelManager>().left != 0)
+        {
+            Debug.Log("Fire");
 
         // Instanciar una pelota
-        FireTemplate pelota = Instantiate(bullet, mano.transform.position, mano.transform.rotation) as FireTemplate;
+            FireTemplate pelota = Instantiate(bullet, mano.transform.position, mano.transform.rotation) as FireTemplate;
 
         // Ponerla en la posición del player
 
         // Dispararla
-        pelota.Fire();
+            pelota.Fire();
+            manager.GetComponent<LevelManager>().left = manager.GetComponent<LevelManager>().left - 1;
+        }
     }
 
     public void Fire2()
     {
-        Debug.Log("Fire2");
+        if (manager.GetComponent<LevelManager>().right != 0)
+        {
+            Debug.Log("Fire2");
 
         // Instanciar una pelota
-        FireTemplate misil = Instantiate(rocketBullet, lanzacohetes.transform.position, lanzacohetes.transform.rotation) as FireTemplate;
+            FireTemplate misil = Instantiate(rocketBullet, lanzacohetes.transform.position, lanzacohetes.transform.rotation) as FireTemplate;
 
         // Ponerla en la posición del player
 
         // Dispararla
-        misil.Fire();
+            misil.Fire();
+            manager.GetComponent<LevelManager>().right = manager.GetComponent<LevelManager>().right - 1;
+        }
     }
 
     public void Jump()
